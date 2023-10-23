a940707ada42240cfceaae5ef9eec2268a9269a9
atoya200 <agustintoya2004@gmail.com>
2023-10-23 15:54:24 -0300

    Agregamos Servicios

--- Angular/Frontend/src/app/app.module.ts
+++ Angular/Frontend/src/app/app.module.ts
@@ -10,6 +10,11 @@ import { RegistradoComponent } from './registrado/registrado.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { BarraDeNavegacionComponent } from './barra-de-navegacion/barra-de-navegacion.component';
 import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { HttpClient } from '@angular/common/http';
+import { LogInService } from './log-in.service';
+import { JuegoService } from './juego.service';
+import { PropuestasService } from './propuestas.service';
 
 @NgModule({
   declarations: [
@@ -19,14 +24,16 @@ import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
     RegistradoComponent,
     EncabezadoComponent,
     BarraDeNavegacionComponent,
-    PieDePaginaComponent
+    PieDePaginaComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [HttpClient, LogInService, JuegoService, PropuestasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
