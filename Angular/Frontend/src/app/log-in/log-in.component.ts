099b4e62860ea6b1d5084bbe57f2a262c958a705
atoya200 <agustintoya2004@gmail.com>
2023-10-30 20:16:09 -0300

    Arreglando login

--- Angular/Frontend/src/app/log-in/log-in.component.ts
+++ Angular/Frontend/src/app/log-in/log-in.component.ts
@@ -9,11 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
-<<<<<<< HEAD
   constructor(private loginService: LogInService, private router: Router) { }
-=======
-  constructor(private servicioRegistro: LogInService, private router: Router) { }
->>>>>>> 30d1f179ea86c737f93af96035a3fe7198f30259
 
   user = ""
   password = ""
@@ -21,39 +17,8 @@ export class LogInComponent {
   userValid: Boolean = true
   passValid: Boolean = true
 
-<<<<<<< HEAD
   signup(){
     this.router.navigate(['/registro']);
-=======
-  unirseAlJuego() { }
-
-  async onSubmit(form: NgForm) {
-
-    /* Va a recibir un array de bools, si tiene dos y ambos están en true, significa que esta todo bien
-    en caso contrario paso algo */
-    console.log(this.servicioRegistro.hacerSolicitudPOST())
-    //let result = await this.servicioRegistro.login(form.value.usuario, form.value.contraseña);
-
-    /* Como estba pensado para poner un aviso de contraseña o usuario incorrecto */
-    /* if (response.length == 2) {
-      this.userValid = response[0]
-      this.userValid = response[1]
-
-    } else {
-      alert("Ocurrio un error al enviar los datos al servidor, por favor repita el proceso")
-    } */
-
- /*    console.log(result[0])
-    if(result[0]) {
-      this.router.navigate(['/inicio']);
-    } else {
-      alert("Usuario invalido")
-    } */
-
-   /*  if (this.userValid && this.passValid) {
-      // Continuar con la vista del administrador
-    } */
->>>>>>> 30d1f179ea86c737f93af96035a3fe7198f30259
   }
 
   login() {
