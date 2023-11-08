a0964116f6f70087c50526592f1128cd4f489e57
atoya200 <agustintoya2004@gmail.com>
2023-11-08 08:26:33 -0300

    Actualización de archivos y prueba de qr

--- Angular/Frontend/src/app/app-routing.module.ts
+++ Angular/Frontend/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { LogInComponent } from './log-in/log-in.component';
 import { RegistradoComponent } from './registrado/registrado.component';
 import { SingUpComponent } from './sing-up/sing-up.component';
 import { DetallesComponent } from './detalles/detalles.component';
-import { UnirseJuegoComponent } from './unirse-juego/unirse-juego.component';
+import { UnirsePropuestaComponent } from './unirse-propuesta/unirse-propuesta.component';
+import { IngresarPseudonimoComponent } from './ingresar-pseudonimo/ingresar-pseudonimo.component';
+import { PruebaJuegoComponent } from './prueba-juego/prueba-juego.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicioSesion', pathMatch: 'full'}, 
@@ -12,7 +14,9 @@ const routes: Routes = [
   { path: 'inicio', component: RegistradoComponent },
   { path: 'registro', component: SingUpComponent },
   { path: 'detalles/:id', component: DetallesComponent },
-  { path: 'unirseJuego', component: UnirseJuegoComponent }
+  { path: 'unirsePropuesta', component: UnirsePropuestaComponent },
+  { path: 'unirsePropuesta/:id', component: IngresarPseudonimoComponent },
+  { path: 'pruebaQR', component: PruebaJuegoComponent }
 ];
 
 @NgModule({
