54bbfd805718d43d64b019af84ebb3b1b9360d1a
atoya200 <agustintoya2004@gmail.com>
2023-11-08 08:19:12 -0300

    Separamos el ingresasr pseudonimmo del ingresar código

---
+++ Angular/Frontend/src/app/ingresar-pseudonimo/ingresar-pseudonimo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-ingresar-pseudonimo',
+  templateUrl: './ingresar-pseudonimo.component.html',
+  styleUrls: ['./ingresar-pseudonimo.component.css']
+})
+export class IngresarPseudonimoComponent {
+
+  constructor(private socketService: SocketService, private router: Router) { }
+
+  codigo: string = ""
+  pseudonimo: string = ""
+
+  ngOnInit() {
+    let urlPagina = this.router.url;
+    let datos = urlPagina.split("/")
+    this.codigo = datos[datos.length -1]
+    console.log('Codigo actual:', this.codigo);
+    this.socketService.setCanal(this.codigo);
+
+  }
+
+  unirseJuego() {
+    this.socketService.unirseJuego(this.codigo, this.pseudonimo);
+  }
+}
